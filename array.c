5b1b115a249245bb187f4494e695217b6e3bb556
vasanthmalik <31505734+vasanthmalik@users.noreply.github.com>
2018-01-10 08:53:35 -0800

    Create array.c

---
+++ array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,k,sum=0;
+printf("enter the elements of the array");
+for(i=0;i<=10;i++)
+{
+scan f(%d",&a[i]);
+}
+printf("enter the no of elements you want to add:");
+scanf(%d",&k);
+for(i=0;i<=k;i++)
+{
+sum=a[i]+sum;
+}
+printf("the sum is%d",sum);
+return 0;
+}
