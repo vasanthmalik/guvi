950a0ad988c12963bf9c874686cdfd99b25412dd
vasanthmalik <31505734+vasanthmalik@users.noreply.github.com>
2018-01-10 08:35:26 -0800

    Create sumnatural.c

---
+++ sumnatural.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+intmain()
+{
+int i,n,sum=0;
+printf("enter the integer");
+scanf("%d".&n);
+for(i=0;i<=n;i++)
+{
+sum+=i;
+}
+printf("the sum of natural numbers is%d",sum);
+return 0;
+}
+
